aca9f3de420e3a7c30c4800cea7e7f5f5f8bd32e
Filippo Campi <filippo.campi@redturtle.it>
2018-03-15 16:38:41 +0100

    [dev] added utils for recover annotations obj + fixed control panel + fixed handler view for rating

--- src/collective/rating/tests/test_setup.py
+++ src/collective/rating/tests/test_setup.py
@@ -3,6 +3,7 @@
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
 from collective.rating.testing import COLLECTIVE_RATING_INTEGRATION_TESTING  # noqa
 
 import unittest
@@ -40,7 +41,7 @@ class TestUninstall(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         self.installer = api.portal.get_tool('portal_quickinstaller')
-        roles_before = api.user.get(userid=TEST_USER_ID).getRoles()
+        roles_before = api.user.get_roles(username=TEST_USER_NAME)
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.installer.uninstallProducts(['collective.rating'])
         setRoles(self.portal, TEST_USER_ID, roles_before)
@@ -56,5 +57,5 @@ class TestUninstall(unittest.TestCase):
             ICollectiveRatingLayer
         from plone.browserlayer import utils
         self.assertNotIn(
-           ICollectiveRatingLayer,
-           utils.registered_layers())
+            ICollectiveRatingLayer,
+            utils.registered_layers())
