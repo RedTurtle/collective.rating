79644b1d63e81b1054f077fcdf9714b2e8b512ce
Filippo Campi <filippo.campi@redturtle.it>
2018-05-25 16:44:26 +0200

    [fix][doc] updated CHANGES + fixed logging

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,7 +5,8 @@ Changelog
 1.0a2 (unreleased)
 ------------------
 
-- Nothing changed yet.
+- Fix logging
+  [eikichi18]
 
 
 1.0a1 (2018-05-23)
--- src/collective/rating/__init__.py
+++ src/collective/rating/__init__.py
@@ -2,5 +2,8 @@
 """Init and utils."""
 from zope.i18nmessageid import MessageFactory
 
+import logging
+
 
 _ = MessageFactory('collective.rating')
+logger = logging.getLogger(__name__)
