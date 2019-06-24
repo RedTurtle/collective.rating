aef2897c530609ed8152a8bd037a44290cfdfedc
Filippo Campi <filippo.campi@redturtle.it>
2019-06-24 14:44:35 +0200

    [dev] added permission for vote a content

--- src/collective/rating/browser/rating_viewlet.py
+++ src/collective/rating/browser/rating_viewlet.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from plone.app.layout.viewlets import ViewletBase
 
 
 class RatingManagerViewlet(ViewletBase):
-
     def rating_is_active(self):
         return self.context.active_rating
 
     def render(self):
         return self.index()
+
+    def can_vote(self):
+        permissions = api.user.get_permissions(user=api.user.get_current())
+        return (
+            permissions["collective.rating: Vote a content"]
+            if permissions.get("collective.rating: Vote a content")
+            else False
+        )
